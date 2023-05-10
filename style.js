213143c0a8df3bc0ded7e3e3224c88084ad9092e
fauzanazhari <fauzan.azharirahman04@gmail.com>
2023-05-10 09:00:16 +0800

    index.html

---
+++ style.js
@@ -0,0 +1,28 @@
+
+var input_nama = prompt("Masukkan Nama:");
+const nama = document.getElementById("nama");
+nama.innerHTML = input_nama;
+
+// var input_namaBelakang = prompt("Masukkan Nama Belakang:");
+// const namaBelakang = document.getElementById("namaBelakang");
+// namaBelakang.innerHTML = input_namaBelakang;
+
+var input_hobi = prompt("Masukkan Hobi:");
+const hobi = document.getElementById("hobi");
+hobi.innerHTML = input_hobi;
+
+var input_film = prompt("Masukkan Film:");
+const film = document.getElementById("film");
+film.innerHTML = input_film;
+
+var input_umur = prompt("Masukkan Umur:");
+const umur = document.getElementById("umur");
+umur.innerHTML = input_umur;
+
+var input_alamat = prompt("Masukkan Alamat:");
+const alamat = document.getElementById("alamat");
+alamat.innerHTML = input_alamat;
+
+var input_email = prompt("Masukkan Email:");
+const email = document.getElementById("email");
+email.innerHTML = input_email;
